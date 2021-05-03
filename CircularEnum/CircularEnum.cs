5cf8e9f97b8568e0b6ea46cef781a66afed11e61
dryan8a@gmail.com <dryan8a@gmail.com>
2021-05-02 19:37:44 -0700

    Add project files.

---
+++ CircularEnum/CircularEnum.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularEnumerator
+{
+   public struct CircularEnum<T> where T : Enum
+   {
+        private T Enumerator;
+        private static int? ElementCount;
+
+        public CircularEnum(T enumerator)
+        {
+            Enumerator = enumerator;
+            InitElementCount();
+        }
+
+        public static void InitElementCount()
+        {
+            if (ElementCount == null)
+            {
+                ElementCount = Enum.GetNames(typeof(T)).Length;
+            }
+        }
+
+        public static implicit operator T(CircularEnum<T> circEnum) => circEnum.Enumerator;
+
+        public static implicit operator CircularEnum<T>(T enumerator) => new CircularEnum<T>(enumerator);
+
+        public static CircularEnum<T> operator +(CircularEnum<T> circEnum, int num)
+        {
+            InitElementCount();
+            int newEnumPos = (int)(object)circEnum.Enumerator + num;
+            if (newEnumPos >= ElementCount)
+            {
+                return new CircularEnum<T>((T)(object)(newEnumPos % ElementCount));
+            }
+            return new CircularEnum<T>((T)(object)newEnumPos);
+        }
+        public static CircularEnum<T> operator ++(CircularEnum<T> circEnum)
+        {
+            InitElementCount();
+            int newEnumPos = (int)(object)circEnum.Enumerator + 1;
+            if (newEnumPos == ElementCount)
+            {
+                return new CircularEnum<T>((T)(object)0);
+            }
+            return new CircularEnum<T>((T)(object)newEnumPos);
+        }
+
+        public static CircularEnum<T> operator -(CircularEnum<T> circEnum, int num)
+        {
+            InitElementCount();
+            int newEnumPos = (int)(object)circEnum.Enumerator - num;
+            if (newEnumPos < 0)
+            {
+                return new CircularEnum<T>((T)(object)(newEnumPos % ElementCount));
+            }
+            return new CircularEnum<T>((T)(object)newEnumPos);
+        }
+        public static CircularEnum<T> operator --(CircularEnum<T> circEnum)
+        {
+            InitElementCount();
+            int newEnumPos = (int)(object)circEnum.Enumerator - 1;
+            if (newEnumPos < 0)
+            {
+                return new CircularEnum<T>((T)(object)(ElementCount - 1));
+            }
+            return new CircularEnum<T>((T)(object)newEnumPos);
+        }
+    }
+}
---
+++ CircularEnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CircularEnumerator
+{
+    enum Stuff
+    {
+        Stuff1,
+        Stuff2,
+        Stuff3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stuff stuff = Stuff.Stuff1;
+            var circularStuff = new CircularEnum<Stuff>(stuff);
+        }
+    }
+}
